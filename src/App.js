0794f774b88840690ff854f5c18c9bd82d81f17b
Kun-Fagyal Zoltan <kunzoltan7707@gmail.com>
2023-02-26 22:27:15 +0100

    upd

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Best from "./components/Best";
+import Featured from "./components/Featured";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Header />
+      <Best />
+      <Featured />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
